e227d8ff080f69cd4da64a7bfe99829208e9a762
andy resta <andy resta>
2016-06-23 07:52:12 +0700

    nambah 'db sia

---
+++ db.sql
@@ -0,0 +1,153 @@
+/*
+SQLyog Enterprise - MySQL GUI v8.18 
+MySQL - 5.6.17 : Database - sia
+*********************************************************************
+*/
+
+/*!40101 SET NAMES utf8 */;
+
+/*!40101 SET SQL_MODE=''*/;
+
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+CREATE DATABASE /*!32312 IF NOT EXISTS*/`sia` /*!40100 DEFAULT CHARACTER SET latin1 */;
+
+USE `sia`;
+
+/*Table structure for table `guru` */
+
+DROP TABLE IF EXISTS `guru`;
+
+CREATE TABLE `guru` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `nama_guru` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `guru` */
+
+insert  into `guru`(`id`,`id_sekolah`,`nama_guru`) values (1,1,'Aji Budianto'),(2,1,'Farian Rahmanto'),(3,2,'Purwanto'),(4,2,'Bambang Marsudi');
+
+/*Table structure for table `hasil_studi` */
+
+DROP TABLE IF EXISTS `hasil_studi`;
+
+CREATE TABLE `hasil_studi` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_kbm` int(11) DEFAULT NULL,
+  `id_kategori_penilaian` int(11) DEFAULT NULL,
+  `nilai` tinyint(4) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `hasil_studi` */
+
+/*Table structure for table `kategori_penilaian` */
+
+DROP TABLE IF EXISTS `kategori_penilaian`;
+
+CREATE TABLE `kategori_penilaian` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `kategori` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=latin1;
+
+/*Data for the table `kategori_penilaian` */
+
+insert  into `kategori_penilaian`(`id`,`id_sekolah`,`kategori`) values (1,1,'UAS'),(2,2,'UAS');
+
+/*Table structure for table `kelas` */
+
+DROP TABLE IF EXISTS `kelas`;
+
+CREATE TABLE `kelas` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `nama_mapel` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `kelas` */
+
+/*Table structure for table `mapel` */
+
+DROP TABLE IF EXISTS `mapel`;
+
+CREATE TABLE `mapel` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `nama_mapel` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `mapel` */
+
+insert  into `mapel`(`id`,`id_sekolah`,`nama_mapel`) values (1,1,'TKJ'),(2,1,'RPLD'),(3,2,'TKJ'),(4,2,'RPLD');
+
+/*Table structure for table `murid` */
+
+DROP TABLE IF EXISTS `murid`;
+
+CREATE TABLE `murid` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `nama_murid` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `murid` */
+
+insert  into `murid`(`id`,`id_sekolah`,`nama_murid`) values (1,1,'Sri Minggat'),(2,1,'Paijo Pujo'),(3,2,'Slamet Wijayanto'),(4,2,'Komar Marudin');
+
+/*Table structure for table `ngajar` */
+
+DROP TABLE IF EXISTS `ngajar`;
+
+CREATE TABLE `ngajar` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `id_guru` int(11) DEFAULT NULL,
+  `id_mapel` int(11) DEFAULT NULL,
+  `id_ta` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `ngajar` */
+
+/*Table structure for table `sekolah` */
+
+DROP TABLE IF EXISTS `sekolah`;
+
+CREATE TABLE `sekolah` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `nama_sekolah` varchar(100) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*Data for the table `sekolah` */
+
+/*Table structure for table `tahun_ajaran` */
+
+DROP TABLE IF EXISTS `tahun_ajaran`;
+
+CREATE TABLE `tahun_ajaran` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `id_sekolah` int(11) DEFAULT NULL,
+  `ta` varchar(20) DEFAULT NULL,
+  `status_ta` tinyint(4) DEFAULT NULL,
+  `keterangan` varchar(10) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=latin1;
+
+/*Data for the table `tahun_ajaran` */
+
+insert  into `tahun_ajaran`(`id`,`id_sekolah`,`ta`,`status_ta`,`keterangan`) values (1,1,'2016/2017',1,'Genap'),(2,1,'2016/2017',0,'Ganjil'),(3,2,'2016/2017',1,'Genap'),(4,2,'2016/2017',0,'Ganjil');
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
